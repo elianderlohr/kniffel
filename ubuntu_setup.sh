530057fdf0eb1d8a56f03c1830c2b10bb7ccccbe
elianderlohr <elias.anderlohr@t-online.de>
2022-09-18 13:27:30 +0200

    fix: update bash

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -10,3 +10,7 @@ sudo apt-get install libmysqlclient-dev
 
 echo "3. Install python packages"
 pip install -r requirements.txt
+
+echo "4. Setup git name and email"
+git config user.name "elias.anderlohr@t-online.de"
+git config user.name "elianderlohr"
